4f250626a643ecff21d61f02a2fc7f2ec50bd3d8
David Shim <davidshim0331@gmail.com>
2024-08-03 15:55:01 -0400

    Update api_service.dart

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -7,9 +7,10 @@ class ApiService {
   ApiService(this.baseUrl);
 
   Future<Map<String, dynamic>> fetchOutput() async {
-    final response = await http.get(Uri.parse('http://100.67.130.194:5000//output'));
+    final response = await http.get(Uri.parse('http://100.67.130.194:5000/'));
 
     if (response.statusCode == 200) {
+      print("Working");
       return json.decode(response.body);
     } else {
       throw Exception('Failed to load output');

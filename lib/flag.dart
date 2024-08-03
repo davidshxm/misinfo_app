f536bf2214f1127f6700661e8b36ebe7659b1cf4
AnnaAardvark <atamlin@uwaterloo.ca>
2024-08-03 06:39:49 -0400

    Create flag.dart

---
+++ lib/flag.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Flag extends StatefulWidget{
+
+  final String userName;
+
+  Flag({required this.userName});
+
+  @override
+  _Flag createState() => _Flag();
+}
+
+class _Flag extends State<Flag>{
+  @override
+
+  void initState (){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      
+    );
+  }
+}

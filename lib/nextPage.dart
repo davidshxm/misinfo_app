55951e63b1514f65d633c95c0defbb5e69be77b2
David Shim <davidshim0331@gmail.com>
2024-08-03 06:25:12 -0400

    Update nextPage.dart

--- lib/nextPage.dart
+++ lib/nextPage.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class NextPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     // Get the screen size
     final size = MediaQuery.of(context).size;
+    List<int> text = [
+      1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6,
+      1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6,
+      1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -33,26 +37,37 @@ class NextPage extends StatelessWidget {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          color: Color(0xffb3c1c5), // Set the background color here
-          child: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.white,
-              ),
-              width: size.width - 50,
-              height: size.height - 100,
-              child: Center(
-                child: Text(
-                  'No Flagged Information',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+      body: Container(
+        color: Color(0xffb3c1c5), // Set the background color here
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.white,
+            ),
+            width: size.width - 50,
+            height: size.height - 100,
+            child: Column(
+              children: [
+                Center(
+                  child: Text(
+                    'No Flagged Information',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        for (var i in text) Text(i.toString())
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),

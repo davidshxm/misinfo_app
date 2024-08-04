e8ae0fcc0faabded91421597525df6d371dfef9a
David Shim <davidshim0331@gmail.com>
2024-08-03 20:00:11 -0400

    COMPLETED MAIN IDEA

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from observer import run_observer_in_thread, result_data
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Start the observer in a separate thread when the application starts
 run_observer_in_thread(result_data)
@@ -11,4 +13,4 @@ def run_scripts():
     return jsonify({"result": result_data["result"]})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
